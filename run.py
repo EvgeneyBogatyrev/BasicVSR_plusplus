e728b5096230aa43f3814f7c6903330dfa8af3b5
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-08-24 17:15:50 +0300

    Add run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ with open("/model/run.sh", "w") as f:
 os.system("chmod 0777 /model/run.sh")
 os.system("/model/run.sh")
 
-os.system("python3 /model/tools/test.py /model/configs/example_config.py /model/...? --save-path /results")
+os.system(f"python3 /model/demo/restoration_video_demo.py /model/configs/basicvsr_plusplus_reds4.py /chkpts/basicvsr_plusplus_reds4.pth /dataset/{video} /results/{video}")
